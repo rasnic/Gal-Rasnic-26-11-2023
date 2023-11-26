b1f76a4f0cc4c448bd315e8746217dc91f862ee0
rasnic <rasnicgal@gmail.com>
2023-11-26 17:35:10 +0200

    navigation

--- abraweather/src/views/favorites/Favorites.js
+++ abraweather/src/views/favorites/Favorites.js
@@ -1,7 +1,18 @@
 import React, {useEffect, useState} from 'react';
-
+import {Button,ButtonGroup} from "reactstrap";
+import { useNavigate } from 'react-router-dom';
 const Favorites = () => {
-    return(<h1>favorites</h1>)
+    const navigate = useNavigate();
+    
+    return(<>
+        <div className="header">
+            <ButtonGroup className="pageButtons" >
+                <Button className='pageButton unselectedBtn' onClick={() => {navigate('/')}}>Home</Button>
+                <Button className='pageButton selectedBtn' disabled>Favorites</Button>
+            </ButtonGroup>
+        </div>
+        <h1>favorites</h1>
+        </>)
 }
 
 export default Favorites;
--- abraweather/src/views/home/Home.js
+++ abraweather/src/views/home/Home.js
@@ -1,7 +1,20 @@
 import React, {useEffect, useState} from 'react';
+import {Button,ButtonGroup} from "reactstrap";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    return(<h1>home</h1>)
+    const navigate = useNavigate();
+    return(<>
+    <div className="header">
+        <ButtonGroup className="pageButtons" >
+            <Button className='pageButton selectedBtn'disabled>Home</Button>
+            <Button className='pageButton unselectedBtn' onClick={() => {navigate('/favorites')}}>Favorites</Button>
+        </ButtonGroup>
+    </div>
+
+    <h1>home</h1>
+    </>
+    )
 }
 
 export default Home;
